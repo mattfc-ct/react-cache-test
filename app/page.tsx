5deb2a730abd676c7d57421c3aa81f575b284dbe
Matt FitzGerald-Chamberlain <matt.fitzgerald-chamberlain@codeandtheory.com>
2025-12-09 14:44:58 -0600

    Update

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,5 @@
 import CachedTestComponent from "@/components/cached-test-component";
 import UncachedTestComponent from "@/components/uncached-test-component";
-import { connection } from "next/server";
 import { Suspense } from "react";
 
 export default async function Home() {
