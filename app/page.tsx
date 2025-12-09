e20d4f52ff538a37b762e353da006659ef126f0f
Matt FitzGerald-Chamberlain <matt.fitzgerald-chamberlain@codeandtheory.com>
2025-12-09 14:44:45 -0600

    Update

--- app/page.tsx
+++ app/page.tsx
@@ -3,18 +3,11 @@ import UncachedTestComponent from "@/components/uncached-test-component";
 import { connection } from "next/server";
 import { Suspense } from "react";
 
-const Connection = async () => {
-    await connection();
-
-    return null;
-}
-
 export default async function Home() {
   
   return (
     <main>
       <Suspense>
-        <Connection />
         <UncachedTestComponent />
         <UncachedTestComponent />
         <hr/>
